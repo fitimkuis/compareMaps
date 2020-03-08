febb8d73793d98aeb3d5780521a6c8419a84c75a
fitim <fitimkuis@gmail.com>
2020-03-08 18:03:21 +0200

    compare maps project

--- src/test/java/MapDiffUtilTest.java
+++ src/test/java/MapDiffUtilTest.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -69,19 +68,49 @@ public class MapDiffUtilTest {
     @Test
     public void testMismatches() {
         final Map<String, String> map1 = new HashMap<String, String>();
-        map1.put("CR001", "GREEN");
+
+        List<String> out = new ArrayList<>();
+        out.add("GREEN");
+        out.add("GREEN");
+        out.add("GREEN");
+        Random r = new Random();
+        for (int i = 0; i < 10; i++){
+            int randomItem = r.nextInt(out.size());
+            String randomElement = out.get(randomItem);
+            map1.put("CR00"+i, randomElement);
+        }
+
+        TreeMap<String, String> sorted1 = new TreeMap<>();
+        sorted1.putAll(map1);
+
+        /*map1.put("CR001", "GREEN");
         map1.put("CR002", "YELLOW");
         map1.put("CR003", "YELLOW");
-        map1.put("CR004", "RED");
+        map1.put("CR004", "RED");*/
 
         final Map<String, String> map2 = new HashMap<String, String>();
-        map2.put("CR001", "GREEN");
+
+        List<String> out1 = new ArrayList<>();
+        out1.add("GREEN");
+        out1.add("YELLOW");
+        out1.add("RED");
+
+        for (int x = 0; x < 10; x++){
+            int randomItem = r.nextInt(out1.size());
+            String randomElement = out1.get(randomItem);
+            map2.put("CR00"+x, randomElement);
+        }
+
+        TreeMap<String, String> sorted2 = new TreeMap<>();
+        sorted2.putAll(map2);
+
+        /*map2.put("CR001", "GREEN");
         map2.put("CR002", "GREEN");
         map2.put("CR003", "GREEN");
-        map2.put("CR004", "YELLOW");
+        map2.put("CR004", "YELLOW");*/
 
         assertFalse("Maps should be unequal", MapDiffUtil.validateEqual(
-                map1, map2, "map1", "map2"));
+                sorted1, sorted2, "map1", "map2"));
 
     }
 }
