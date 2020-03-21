3599712db17bc3f1531d39709b63e822d374c001
fitim <fitimkuis@gmail.com>
2020-03-21 19:26:35 +0200

    totally dynamic read xml

--- src/main/java/ReadXML.java
+++ src/main/java/ReadXML.java
@@ -1,9 +1,12 @@
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.*;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,6 +14,7 @@ import java.util.List;
 
 
 public class ReadXML {
+
 
     public static void main(String argv[]) {
         try {
@@ -23,45 +27,187 @@ public class ReadXML {
             NodeList nList = doc.getElementsByTagName("report");
 
             int cntNlist = nList.getLength();
+            int tagCounter = 0;
+            DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder();
+            Document document = docBuilder.parse("C:\\Users\\fitim\\IdeaProjects\\compareMaps\\src\\main\\java\\stuff.xml");
+
+
+            //NodeList nodes = doc.getElementsByTagName("reports");
+            //System.out.println("\nHere you go => Total # of Elements: " + nodes.getLength());
+
+            List<String> tags = new ArrayList<>();
+            NodeList nodeList = document.getElementsByTagName("*");
+            //get count of R tag names in a node
+            int countReport = 0;
+            for(int i = 0; i < nodeList.getLength(); i++){
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    if (node.getNodeName().equals("report")){
+                        countReport++;
+                    }
+                    if (node.getNodeName().contains("R")){
+                        tags.add(node.getNodeName());
+                        tagCounter++;
+                    }
+                    if(countReport > 1){
+                        break;
+                    }
+                    //System.out.println(node.getNodeName());
+                }
+
+            }
+            //get all R tag names to list
+            /*for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    if (node.getNodeName().contains("R")){
+                        tags.add(node.getNodeName());
+                        tagCounter++;
+                    }
+                    //System.out.println(node.getNodeName());
+                }
+            }*/
+
+            //dynamic list for tag values
+            HashMap<String,ArrayList<String>> maps1 = new HashMap<String, ArrayList<String>>();
+            String tag = tags.get(0);
+            String latest = tags.get(tags.size() - 1);
+            latest = latest.replace("R","");
+            int latestTag = Integer.parseInt(latest);
+            tag = tag.replace("R","");
+            int startTag = Integer.parseInt(tag);
+            for (int i = 0; i <= tagCounter-1; i++) {
+                maps1.put("r" + startTag, new ArrayList<>());
+                startTag++;
+            }
+
+            startTag = Integer.parseInt(tag);
+            //get R values from xml
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                //System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+                    for (int i = 0; i <= tagCounter-1; i++) {
+                        maps1.get("r" + startTag).add(eElement.getElementsByTagName("R" + startTag).item(0).getTextContent());
+                        startTag++;
+                        if (startTag >= latestTag + 1) {
+                            startTag = Integer.parseInt(tag);
+                        }
+                    }
+                }
+            }
+            startTag = Integer.parseInt(tag);
+            //TODO print out and calculate
+            HashMap<Integer,ArrayList<Integer>> maps2 = new HashMap<Integer, ArrayList<Integer>>();
+            for (int i = 0; i <= tagCounter-1; i++){
+                maps2.put(startTag, new ArrayList<>());
+                startTag++;
+            }
+            //add maps1 values to maps2 as integer
+            startTag = Integer.parseInt(tag);
+            String val = maps1.get("r"+startTag).get(0);
+            for (int i = 0; i < nList.getLength(); i++){
+
+                for (int j = 0; j <= tagCounter-1; j++) {
+                    if (maps1.get("r"+startTag).get(i).equals("")) {
+                        maps2.get(startTag).add(0);
+                        //maps2.get(startTag).add(Integer.parseInt(maps1.get("r" + startTag).get(0)));
+                    }
+                    else{
+                        maps2.get(startTag).add(Integer.parseInt(maps1.get("r" + startTag).get(i)));
+                    }
+                    startTag++;
+                    if (startTag >= latestTag + 1) {
+                        startTag = Integer.parseInt(tag);
+                    }
+                }
+            }
+
+            startTag = Integer.parseInt(tag);
+            System.out.println("Node values");
+            int sumCounter = 1;
+            int nodesSum = 0;
+            int getCounter = 0;
+            int tempSum = 0;
+            for (int i = 0; i < nList.getLength(); i++) {
+                for (int j = 0; j <= tagCounter-1; j++) {
+                    if (maps2.get(startTag).get(i) != 0) {
+                        nodesSum += maps2.get(startTag).get(i);
+                        tempSum += maps2.get(startTag).get(i);
+                    }
+                    else{
+                        nodesSum += 0;
+                        tempSum += 0;
+                    }
+                    //System.out.println("node R"+startTag+ " sum: "+nodesSum);
+                    System.out.println("node R"+startTag+ " value: "+maps2.get(startTag).get(getCounter));
+                    if (sumCounter % tagCounter == 0){
+                        System.out.println("nodes sum: "+nodesSum);
+                        getCounter++;
+                        nodesSum = 0;
+                    }
+                    startTag++;
+                    if (startTag >= latestTag + 1) {
+                        startTag = Integer.parseInt(tag);
+                    }
+                    sumCounter++;
+                }
+            }
+
+            System.out.println("R Node sum values: "+tempSum);
+
+
+/*
+            int tagStart = 0;
 
             //dynamic way
-            HashMap<String,ArrayList<String>> maps=new HashMap<String, ArrayList<String>>();
+            HashMap<String,ArrayList<String>> maps = new HashMap<String, ArrayList<String>>();
             int start = 4;
             for (int i = 0; i <= cntNlist; i++) {
                 maps.put("r" + start, new ArrayList<>());
                 start++;
             }
 
-            start = 4;
-
             List<String> r4 = new ArrayList<>();
             List<String> r5 = new ArrayList<>();
             List<String> r6 = new ArrayList<>();
             List<String> r7 = new ArrayList<>();
+
+            start = 4;
 
             for (int temp = 0; temp < nList.getLength(); temp++) {
                 Node nNode = nList.item(temp);
                 System.out.println("\nCurrent Element :" + nNode.getNodeName());
                 if (nNode.getNodeType() == Node.ELEMENT_NODE) {
                     Element eElement = (Element) nNode;
-                    System.out.println("R4 : " + eElement.getElementsByTagName("R4").item(0).getTextContent());
+
+                    System.out.println("R4 : " + eElement.getElementsByTagName(tags.get(tagStart)).item(0).getTextContent());
                     r4.add(eElement.getElementsByTagName("R4").item(0).getTextContent());
                     maps.get("r"+start).add(eElement.getElementsByTagName("R"+start).item(0).getTextContent());
                     start++;
-                    System.out.println("R5 : " + eElement.getElementsByTagName("R5").item(0).getTextContent());
+                    tagStart++;
+                    System.out.println("R5 : " + eElement.getElementsByTagName(tags.get(tagStart)).item(0).getTextContent());
                     maps.get("r"+start).add(eElement.getElementsByTagName("R"+start).item(0).getTextContent());
                     start++;
+                    tagStart++;
                     r5.add(eElement.getElementsByTagName("R5").item(0).getTextContent());
-                    System.out.println("R6 : " + eElement.getElementsByTagName("R6").item(0).getTextContent());
+                    System.out.println("R6 : " + eElement.getElementsByTagName(tags.get(tagStart)).item(0).getTextContent());
                     maps.get("r"+start).add(eElement.getElementsByTagName("R"+start).item(0).getTextContent());
                     start++;
+                    tagStart++;
                     r6.add(eElement.getElementsByTagName("R6").item(0).getTextContent());
-                    System.out.println("R7 : " + eElement.getElementsByTagName("R7").item(0).getTextContent());
+                    System.out.println("R7 : " + eElement.getElementsByTagName(tags.get(tagStart)).item(0).getTextContent());
                     r7.add(eElement.getElementsByTagName("R7").item(0).getTextContent());
                     maps.get("r"+start).add(eElement.getElementsByTagName("R"+start).item(0).getTextContent());
                     start = 4;
+                    tagStart = 0;
                 }
             }
+
+            //System.out.println(maps1.get("R4"));
 
             int node4 = 0;
             int node5 = 0;
@@ -133,6 +279,7 @@ public class ReadXML {
 
             int sum = nodeR4+nodeR5+nodeR6+nodeR7;
             System.out.println("number of R nodes :"+counter+" nodes SUM: "+sum);
+            */
         } catch (Exception e) {
             e.printStackTrace();
         }
